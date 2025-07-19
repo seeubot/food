c737d3eb25bc8ff9148b93335e8951e5cb394997
seeubot <155578467+seeubot@users.noreply.github.com>
2025-07-19 08:56:24 +0530

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,24 +1,23 @@
-    const mongoose = require('mongoose');
-    const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-    const adminSchema = new mongoose.Schema({
-        username: { type: String, required: true, unique: true },
-        password: { type: String, required: true }
-    });
+const adminSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
 
-    // Hash password before saving
-    adminSchema.pre('save', async function(next) {
-        if (this.isModified('password')) {
-            this.password = await bcrypt.hash(this.password, 10);
-        }
-        next();
-    });
+// Hash password before saving
+adminSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
 
-    // Method to compare password
-    adminSchema.methods.comparePassword = function(candidatePassword) {
-        return bcrypt.compare(candidatePassword, this.password);
-    };
+// Method to compare password
+adminSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
-    module.exports = mongoose.model('Admin', adminSchema);
-    ```
+module.exports = mongoose.model('Admin', adminSchema);
 
