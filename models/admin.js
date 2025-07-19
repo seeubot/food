f9ae6da96379edd7fb51e1207b405422e1d1e642
seeubot <155578467+seeubot@users.noreply.github.com>
2025-07-19 08:39:34 +0530

    Create admin.js

---
+++ models/admin.js
@@ -0,0 +1,24 @@
+    const mongoose = require('mongoose');
+    const bcrypt = require('bcryptjs');
+
+    const adminSchema = new mongoose.Schema({
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
+    });
+
+    // Hash password before saving
+    adminSchema.pre('save', async function(next) {
+        if (this.isModified('password')) {
+            this.password = await bcrypt.hash(this.password, 10);
+        }
+        next();
+    });
+
+    // Method to compare password
+    adminSchema.methods.comparePassword = function(candidatePassword) {
+        return bcrypt.compare(candidatePassword, this.password);
+    };
+
+    module.exports = mongoose.model('Admin', adminSchema);
+    ```
+
